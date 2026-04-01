5e8999e7b40652d08a2245f2f41c7d685d709b33
Allan Ritchie <allan.ritchie@gmail.com>
2026-04-01 17:49:18 -0400

    Remove unnecessary builder

--- src/Shiny.Maui.Scheduler/MauiAppBuilderExtensions.cs
+++
@@ -1,9 +0,0 @@
-namespace Shiny.Maui.Scheduler;
-
-public static class MauiAppBuilderExtensions
-{
-    public static MauiAppBuilder UseShinyScheduler(this MauiAppBuilder builder)
-    {
-        return builder;
-    }
-}
